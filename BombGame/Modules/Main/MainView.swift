b16b0bbba9cb96fc687d697e32435e373b03eb84
Ojidaemo <vit.martsinovich@gmail.com>
2023-08-07 19:27:16 +0300

    Add UI elements to Main, Rules and Categories views

--- BombGame/Modules/Main/MainView.swift
+++ BombGame/Modules/Main/MainView.swift
@@ -3,8 +3,41 @@ import SwiftUI
 struct MainView: View {
     @ObservedObject var viewModel: MainViewModel
     
+    @State private var startGame = "Старт игры"
+    @State private var proceedGame = "Продолжить"
+    @State private var categories = "Категории"
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            ZStack {
+                BackgroundView()
+                VStack {
+                    Text(Resources.Text.mainScreenTitle)
+                        .font(.system(size: 32, weight: .heavy))
+                        .multilineTextAlignment(.center)
+                        .foregroundColor(.black)
+                    Text(Resources.Text.bomb)
+                        .font(.system(size: 48, weight: .heavy))
+                        .multilineTextAlignment(.center)
+                        .foregroundColor(Resources.Colors.mainPurple)
+                    Image(Resources.Image.bomb)
+                        .resizable()
+                        .scaledToFit()
+                    VStack {
+                        ActionButton(text: $startGame, onTapAction: viewModel.startGamePressed)
+                        ActionButton(text: $proceedGame, onTapAction: viewModel.proceedGamePressed)
+                        ActionButton(text: $categories, onTapAction: viewModel.categoriesPressed)
+                    }
+                    HStack {
+                        CircleButton(imageName: Resources.Image.settings, onTapAction: viewModel.settingsPressed)
+                        Spacer()
+                        CircleButton(imageName: Resources.Image.helpImage, onTapAction: viewModel.helpPressed)
+                    }
+                    .padding(.horizontal)
+                }
+                .padding(.top, 50)
+            }
+        }
     }
 }
 
--- BombGame/Modules/Main/MainViewModel.swift
+++ BombGame/Modules/Main/MainViewModel.swift
@@ -2,4 +2,23 @@ import Foundation
 
 final class MainViewModel: ObservableObject {
     
+    func startGamePressed() {
+        print("start game pressed")
+    }
+    
+    func proceedGamePressed() {
+        print("proceedGamePressed")
+    }
+    
+    func categoriesPressed() {
+        print("categoriesPressed")
+    }
+    
+    func settingsPressed() {
+        print("settingsPressed")
+    }
+    
+    func helpPressed() {
+        print("settingsPressed")
+    }
 }
