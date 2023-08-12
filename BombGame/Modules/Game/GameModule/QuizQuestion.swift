180d593dde1818c1ef8dc95899f42ba3d0b141a7
Yan Boyko <yanboyko21@gmail.com>
2023-08-12 16:45:15 +0300

    fixes

--- BombGame/Modules/Game/GameModule/QuizQuestion.swift
+++ BombGame/Modules/Game/GameModule/QuizQuestion.swift
@@ -11,7 +11,7 @@ struct QuizQuestion: Codable {
     let category: QuestionsBox.CategoryName
     let question: String
   
-    static func getRandomQuestion(category: QuestionsBox.CategoryName) -> QuizQuestion {
+    static func getRandomQuestion(categories: [QuestionsBox.CategoryName]) -> QuizQuestion {
         QuizQuestion.quizData.randomElement() ?? QuizQuestion(category: .aboutLife, question: "Не удалось сгенерировать вопрос")
     }
   
